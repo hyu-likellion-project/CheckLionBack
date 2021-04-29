8f2550bc38cb4724a735c8cea3ac5e5fd7e81abb
hoon4233 <wogns3141@gmail.com>
2021-04-29 19:59:39 +0900

    feat#2 User 모델 설계

--- User/admin.py
+++ User/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Users
 
-# Register your models here.
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('email', 'level', 'register_date')
+
+admin.site.register(Users, UsersAdmin)
